6bcbb75da031e73ecb53bfde423345280e5a97b2
shubham <shubhamsinghmor2312@gmail.com>
2024-08-02 22:14:33 +0530

    changes done

--- index.js
+++ index.js
@@ -6,9 +6,34 @@ const path=require("path");
 let port=8080;
 
 app.listen(port,()=>{
-    console.log("port is listening");
+    console.log(`port is listening ${port}`);
 })
-
+let users=[
+    {
+        username:"shubhamm__1223",
+        bio:"shubham",
+        followers:50,
+        following:90,
+        Posts:0,
+        Followed_by:"harsh,yash"
+    },
+    {
+        username:"dushyant_vashisht",
+        bio:"Dushyant",
+        followers:80,
+        following:110,
+        Posts:0,
+        Followed_by:"harsh,shubham"
+    },
+    {
+        username:"harsh_9560",
+        bio:"harsh",
+        followers:10,
+        following:30,
+        Posts:0,
+        Followed_by:"shubham,dushyant"
+    }
+]
 
 app.use(express.urlencoded({extended:true}))
 
@@ -26,8 +51,19 @@ app.get("/",(req,res)=>{
 
 app.post("/profile",(req,res)=>{
     let {username,password}=req.body;
-    console.log(username,password);
-    res.render("profile.ejs")
+    console.log(username)
+    
+    let user=users.find((u)=>username===u.username);
+    if(user){
+        let newContent=req.body.content;
+        if(newContent){
+            user.content=newContent;
+        }
+        res.render("profile.ejs",{user});
+    }
+    else{
+        res.status(404).send("user not found")
+    }
 })
 app.get("/home",(req,res)=>{
     res.render("home.ejs")
